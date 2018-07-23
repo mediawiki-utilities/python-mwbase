9c12bcb4a5b5b2fee28476e047855dca50b3867c
halfak <aaron.halfaker@gmail.com>
2018-07-23 19:46:49 +0200

    Switch use of getattr to getattribute.

--- mwbase/attr_dict.py
+++ mwbase/attr_dict.py
@@ -3,13 +3,13 @@ from collections import OrderedDict
 
 class AttrDict(OrderedDict):
     def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
+        super(OrderedDict, self).__init__(*args, **kwargs)
 
-    def __getattr__(self, attr):
-        if attr not in self:
-            raise AttributeError(attr)
-        else:
+    def __getattribute__(self, attr):
+        if attr in self:
             return self[attr]
+        else:
+            return super(OrderedDict, self).__getattribute__(attr)
 
     def __setattr__(self, attr, value):
         self[attr] = value
